95510d9ae6a4d4ae46bf2b3ebe94229fbf84bf99
Salman <smd397452@gmail.com>
2025-06-24 10:08:17 +0530

    fix women to men

--- frontend/src/Components/Popular/Popular.jsx
+++ frontend/src/Components/Popular/Popular.jsx
@@ -1,33 +1,4 @@
 /* eslint-disable no-unused-vars */
-// import React from "react";
-// import "./Popular.css";
-// import data from "../Assets/data";
-// import Item from "../Item/Item";
-// const Popular = () => {
-//   return (
-//     <div className="popular">
-//       <h1>POPULAR IN WOMEN</h1>
-//       <hr />
-//       <div className="popular-item">
-//         {data.map((item, i) => {
-//           return (
-//             <Item
-//               key={i}
-//               id={item.id}
-//               name={item.name}
-//               image={item.image}
-//               new_price={item.new_price}
-//               old_price={item.old_price}
-//             />
-//           );
-//         })}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Popular;
-
 import React, { useEffect, useState } from "react";
 import "./Popular.css";
 import Item from "../Item/Item";
@@ -42,7 +13,7 @@ const Popular = () => {
 
   return (
     <div className="popular">
-      <h1>POPULAR IN WOMEN</h1>
+      <h1>POPULAR IN MEN</h1>
       <hr />
       <div className="popular-container">
         {popularProducts.map((item, i) => (
